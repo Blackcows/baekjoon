ae3e3596e47e7b1622e63c42e795e098cc01c11e
Gukjin Choi <44183228+Blackcows@users.noreply.github.com>
2024-08-22 09:27:34 +0900

    [Silver III] Title: 퇴사, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+schedule = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0 for _ in range(n+1)]
+
+for i in range(n):
+    if i + schedule[i][0] > n:
+        continue
+    for j in range(i+schedule[i][0], n+1):
+        if dp[j] < dp[i] + schedule[i][1]:
+            dp[j] = dp[i] + schedule[i][1]
+
+print(dp[-1])
